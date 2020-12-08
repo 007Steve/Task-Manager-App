7be9a98a14fc85e3fdd3578049f7f8867ce6f18e
007steve <stephen.plummer1.sp@gmail.com>
2020-12-08 15:51:44 -0500

    added home pages

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import"../styles/Home.css"
+function Home() {
+    return (
+        <div className="home">
+            <h4>home</h4>
+        </div>
+    )
+}
+
+export default Home
